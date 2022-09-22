ad5891572b5e39e67167de8ecd4bbe9605e28226
Ayesha Aamir <ayeshaaamir38@gmail.com>
2022-09-23 01:39:06 +0500

    Time: 66 ms (87.50%), Space: 41.8 MB (78.13%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.js
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var minCostToMoveChips = function(position) {
-    let even = 0;
-    let odd = 0;
+   let even = 0;
+   let odd = 0;
     
-    for(let i = 0; i < position.length; i++) {
-        if(position[i] % 2) ++odd;
-        else ++even;
+    for (let i = 0; i < position.length; i++){
+        position[i] % 2 === 0 ? even++ : odd++;
     }
     
-    return Math.min(odd, even);
+    return Math.min(even, odd);
+
 };
