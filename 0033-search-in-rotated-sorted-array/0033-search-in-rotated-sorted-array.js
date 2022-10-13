edd37f3994ec99b3c9986e3d8e9180a1e00e92f1
Ayesha Aamir <ayeshaaamir38@gmail.com>
2022-10-14 02:57:15 +0500

    Time: 104 ms (35.22%), Space: 42.1 MB (53.97%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,66 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let pivot = findPivot(nums);
+    
+    //if pivot not found
+    if(pivot == -1) {
+        //normal binary search
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+    
+    //if pivot found, we have to find 2 asc sorted arrays
+    if(nums[pivot] == target) {
+        return pivot;
+    }
+    
+    if(target >= nums[0]) {
+        return binarySearch(nums, target, 0, pivot - 1);
+    }
+    
+    return binarySearch(nums, target, pivot + 1, nums.length - 1);
+};
+
+let findPivot = (nums) => {
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while(start <= end) {
+        let mid = parseInt(start + (end - start) / 2);
+        
+        //use 4 cases
+        if(mid < end && nums[mid] > nums[mid + 1]) {
+            return mid;
+        }
+        if(mid > start && nums[mid] < nums[mid - 1]) {
+            return mid - 1;
+        }
+        if(nums[mid] <= nums[start]) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+};
+
+let binarySearch = (nums, target, start, end) => {
+    while(start <= end) {
+        let mid = parseInt(start + (end - start) / 2);
+        
+        if(target < nums[mid]) {
+            end = mid - 1;
+        }
+        else if(target > nums[mid]) {
+            start = mid + 1;
+        }
+        else {
+            return mid;
+        }
+    }
+    return -1;
+};
