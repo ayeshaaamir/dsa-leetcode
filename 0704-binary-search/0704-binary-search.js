65cb680a20c935b77c2163396e3315e7d916347a
Ayesha Aamir <ayeshaaamir38@gmail.com>
2022-10-19 21:12:46 +0500

    Time: 115 ms (28.59%), Space: 45.1 MB (54.81%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while(start <= end) {
+        let mid = parseInt(start + (end - start) / 2);
+        
+        if(nums[mid] == target) {
+            return mid;
+        }
+        else if(nums[mid] > target) {
+            end = mid - 1;
+        }
+        else if(nums[mid] < target) {
+            start = mid + 1;
+        }
+    }
+    return - 1;
+};
