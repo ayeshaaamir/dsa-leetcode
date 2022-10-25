2139d7a57917da3b83b3a8cd57e1d4062867c8c1
Ayesha Aamir <ayeshaaamir38@gmail.com>
2022-10-25 20:44:49 +0500

    Time: 75 ms (81.71%), Space: 41.8 MB (73.17%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -6,10 +6,13 @@ var check = function(nums) {
     let n = nums.length;
     let count = 0;
     
-    for(let i = 0; i < n; i++) {
-        if(nums[i] > nums[(i + 1) % n]) {
+    for(let i = 1; i < n; i++) {
+        if(nums[i - 1] > nums[i]) {
             count++;
         }
+    }
+    if(nums[n - 1] > nums[0]){
+        count++;
     }
     return (count <= 1);
 };
