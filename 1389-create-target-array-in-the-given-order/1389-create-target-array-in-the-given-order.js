2fe0bd35556d399be031081e1eec0d796964d7de
Ayesha Aamir <ayeshaaamir38@gmail.com>
2022-11-23 21:46:48 +0500

    Time: 106 ms (26.82%), Space: 42.4 MB (20.36%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -4,9 +4,20 @@
  * @return {number[]}
  */
 var createTargetArray = function(nums, index) {
+    // brute force
+    // let target = new Array(index.length);
+    // for(let i = 0; i < index.length; i++) {
+    //     for(let j = target.length - 1; j > index[i]; j--) {
+    //         target[j] = target[j - 1];
+    //     }
+    //     target[index[i]] = nums[i];
+    // }
+    // return target;
+    
+    //optimized
     let target = new Array(index.length);
     for(let i = 0; i < index.length; i++) {
-        for(let j = target.length - 1; j > index[i]; j--) {
+        for(let j = i; j > index[i]; j--) {
             target[j] = target[j - 1];
         }
         target[index[i]] = nums[i];
