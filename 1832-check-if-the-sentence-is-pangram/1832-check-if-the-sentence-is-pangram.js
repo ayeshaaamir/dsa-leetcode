de4e7c0a172defbbd55ca79abf3dcee4c3c78a6c
Ayesha Aamir <ayeshaaamir38@gmail.com>
2022-11-23 22:28:29 +0500

    Time: 97 ms (57.12%), Space: 41.9 MB (95.38%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -3,10 +3,23 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    const sentenceToArray = sentence.split('')
-    const newSet = new Set(sentenceToArray);
-
-    if (newSet.size < 26) return false;
+    // brute force
+    // let array = new Array(26).fill(false);
+    // for(let i = 0; i < sentence.length; i++) {
+    //     array[sentence.charAt(i)-97] = true;
+    // }
+    // for(let i = 0; i < 26; i++) {
+    //     if(array[i] == false) {
+    //         return false;
+    //     }
+    // }
+    // return true;
     
-    return true;
+    //  optimize using set
+     const sentenceToArray = sentence.split('')
+     const newSet = new Set(sentenceToArray);
+
+     if (newSet.size < 26) return false;
+
+     return true;
 };
