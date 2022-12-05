b542951b16c71b6a36beaa87f3221002c662fe04
Ayesha Aamir <ayeshaaamir38@gmail.com>
2022-12-05 20:12:04 +0500

    Time: 280 ms (38.27%), Space: 66.7 MB (7.68%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -5,9 +5,18 @@
  */
 var findKthLargest = function(nums, k) {
     //brute force
-    nums.sort((a, b) => {return a - b}).reverse();
-    return nums[k - 1];
+    // nums.sort((a, b) => {return a - b}).reverse();
+    // return nums[k - 1];
     
     //optimized
+    let maxPriorityQueue = new MaxPriorityQueue();
+    
+    nums.forEach((num) => maxPriorityQueue.enqueue(num));
+    
+    while(k > 1) {
+        maxPriorityQueue.dequeue();
+        k--;
+    }
+    return maxPriorityQueue.front().element;
     
 };
